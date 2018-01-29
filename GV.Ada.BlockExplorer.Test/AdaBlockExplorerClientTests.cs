467c364609bc0e6fc2b1873f2905673505d477c9
SamZ <samz.mail.me@gmail.com>
2018-01-29 18:32:27 +0300

    addition tests api alient

--- GV.Ada.BlockExplorer.Test/AdaBlockExplorerClientTests.cs
+++ GV.Ada.BlockExplorer.Test/AdaBlockExplorerClientTests.cs
@@ -3,11 +3,11 @@ using Xunit;
 
 namespace GV.Ada.BlockExplorer.Test
 {
-    public class AdaBlockExplorerClientTests
+	public class AdaBlockExplorerClientTests
 	{
-        [Fact]
-        public async void GetBlocksPageLatestTest()
-        {
+		[Fact]
+		public async void GetBlocksPageLatestTest()
+		{
 			var service = new AdaBlockExplorerClient();
 			var result = await service.GetBlocksPage();
 			Assert.NotNull(result);
@@ -28,5 +28,40 @@ namespace GV.Ada.BlockExplorer.Test
 			var result = await service.GetBlockTransactions("3dc7a73a3e27384cfc4b0d01045e072a1c80fdd9ca628a3f03ebe51e4028158a");
 			Assert.NotNull(result);
 		}
+
+		[Fact]
+		public async void GetEpochPageTest()
+		{
+			var service = new AdaBlockExplorerClient();
+			var result = await service.GetEpochPage(24);
+			Assert.NotNull(result);
+		}
+
+		[Fact]
+		public async void GetLatestBlockTest()
+		{
+			var service = new AdaBlockExplorerClient();
+			var result = await service.GetLatestBlock();
+			Assert.NotNull(result);
+		}
+
+		[Fact]
+		public async void GetSlotCountByEpochTest()
+		{
+			var service = new AdaBlockExplorerClient();
+			var result = await service.GetSlotCountByEpoch(24);
+			Assert.Equal(21599, result);
+		}
+
+		[Fact]
+		public async void GetTransactionTest()
+		{
+			var service = new AdaBlockExplorerClient();
+			var result = await service.GetTransaction("02712610f05e284db32c37eda9c1212f0a719585a0b4f8f822af669eb2b3d384");
+			Assert.NotNull(result);
+		}
+
+
+
 	}
 }
